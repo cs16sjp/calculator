c8e772405041b257c1ed756cadaca043e5ce19c0
Stacey Powell <stacey.powell@asos.com>
2018-06-27 15:26:36 +0100

    bug fix

--- Program.cs
+++ Program.cs
@@ -4,9 +4,13 @@ namespace calculatorprogram
 {
     class Program
     {
+        public static void SomeImportantBugFix(){
+            //fixes all the bugs
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Calculator Version 1.0");
+            SomeImportantBugFix();
         }
     }
 }
