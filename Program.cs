d49fd2d2678a3dea2ee90e308904230ebe844d91
Stacey Powell <stacey.powell@asos.com>
2018-06-27 14:30:49 +0100

    Second Commit

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace CalculatorProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Calculator Version 1.0");
+            while(true){
+                Console.WriteLine("Endless Loooooop!");
+            }
         }
     }
 }
